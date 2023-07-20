43509da8e658aab88e2b0b70d98427088f1ef769
Wei Wei <2312052806@qq.com>
2023-07-20 16:11:00 +0800

    start

---
+++ src/createWin.js
@@ -0,0 +1,26 @@
+import { BrowserWindow } from "electron"
+import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
+
+export default async (options) => {
+    options.webPreferences = {
+        // Use pluginOptions.nodeIntegration, leave this alone
+        // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+        nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+        contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
+    }
+
+    // Create the browser window.
+    const win = new BrowserWindow(options)
+
+    if (process.env.WEBPACK_DEV_SERVER_URL) {
+        // Load the url of the dev server if in development mode
+        await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+        if (!process.env.IS_TEST) win.webContents.openDevTools()
+    } else {
+        createProtocol('app')
+        // Load the index.html when not in development
+        win.loadURL('app://./index.html')
+    }
+
+    return win
+}
